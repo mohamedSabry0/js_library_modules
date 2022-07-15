d32b9c1f11700bb49b2a3686fa948d975ef3a31e
mohamedSabry0 <mohamedsabry3023@gmail.com>
2022-07-15 17:27:02 +0200

    add date

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 import { DateTime } from './modules/luxon.js';
 import { BookList } from './modules/list.js';
-import { addBtn } from './modules/selectors.js';
+import { addBtn, dateBox } from './modules/selectors.js';
 import { Handler } from './modules/handlers.js';
-
-// console.log('test')
-// console.log(Handler);
 
 const bookList = new BookList();
 const handler = new Handler(bookList);
 addBtn.addEventListener('click', handler);
 
+
+dateBox.textContent = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+
 // TODO: list functionality
 // TODO: add new functionality
+// TODO: display functionality
 // TODO: remove functionality
 // TODO: storage functionality
-// TODO: date functionality
+
 // TODO: nav functionality
+// TODO: styling
